a8e294e13d430d9e5232141d2258e46a28720779
KinzaKhurram123 <kinzakhurram84@gmail.com>
2026-03-02 00:38:19 +0500

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  forgetPassword,
+  conformationPassword,
+  resetPassword,
+  getProfile,
+  updateProfile,
+} = require("../controllers/authController");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/forget_password", forgetPassword);
+router.post("/checkOTP", conformationPassword);
+router.post("/reset_password", resetPassword);
+router.post("/edit_profile", updateProfile);
+router.post("/get_profile", getProfile);
+
+module.exports = router;
